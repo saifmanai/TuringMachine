ff7a35dd18b5c2b6be0835a310e78304b4876b67
MelaniaNitu <melania.nitu@yahoo.com>
2014-03-01 14:00:17 +0200

    Functionalitate butoane +/-

--- Interface.py
+++ Interface.py
@@ -2,37 +2,37 @@ import wx
 import wx.grid as gridlib
 
 class Interface(wx.Frame):
-
     def __init__(self,parent,id):
         wx.Frame.__init__(self,parent,id,'Machine Turing',size=(740,400), style= wx.SYSTEM_MENU | wx.CAPTION | wx.CLOSE_BOX)
         hbox = wx.BoxSizer(wx.HORIZONTAL)
         self.SetSizer(hbox)
         
-        pnl1 = wx.Panel(self, -1, style=wx.NO_BORDER,size=(300,0))
-        hbox.Add(pnl1,0, wx.EXPAND | wx.RIGHT, 4)
+        self.pnl1 = wx.Panel(self, -1, style=wx.NO_BORDER,size=(300,0))
+        hbox.Add(self.pnl1,0, wx.EXPAND | wx.RIGHT, 4)
         
-        vbox = wx.BoxSizer(wx.VERTICAL)
-        pnl1.SetSizer(vbox)
+        self.vbox = wx.BoxSizer(wx.VERTICAL)
+        self.pnl1.SetSizer(self.vbox)
+
+        pnl_btn = wx.Panel(self.pnl1, -1, style=wx.NO_BORDER)
+        self.vbox.Add(pnl_btn,0,wx.EXPAND | wx.ALL, 10)
         
-        tape = wx.TextCtrl(pnl1)
-        vbox.Add(tape,0,wx.EXPAND | wx.ALL, 10)
-
-        pnl_btn = wx.Panel(pnl1, -1, style=wx.NO_BORDER)
-        vbox.Add(pnl_btn,0,wx.EXPAND | wx.ALL, 10)
-
+        tape = wx.TextCtrl(self.pnl1)
+        self.vbox.Add(tape,0,wx.EXPAND | wx.ALL, 10)
+        
         btn_box = wx.BoxSizer(wx.HORIZONTAL)
         pnl_btn.SetSizer(btn_box)
 
         add_btn=wx.Button(pnl_btn,label="+")
+        self.Bind(wx.EVT_BUTTON, self.add_tape, add_btn)
         btn_box.Add(add_btn,0,wx.EXPAND | wx.RIGHT, 10)
-
+        
         remove_btn=wx.Button(pnl_btn,label="-")
+        self.Bind(wx.EVT_BUTTON, self.remove_tape, remove_btn)
         btn_box.Add(remove_btn,0,wx.EXPAND | wx.RIGHT, 10)
 
         start_btn=wx.Button(pnl_btn,label="Start")
         btn_box.Add(start_btn,0,wx.EXPAND | wx.RIGHT, 10)
-
-
+        
         myGrid=gridlib.Grid(self)
         myGrid.CreateGrid(30,5)
         myGrid.SetRowLabelSize(0)
@@ -42,15 +42,26 @@ class Interface(wx.Frame):
         myGrid.SetColLabelValue(2,"Write Values")
         myGrid.SetColLabelValue(3,"Direction")
         myGrid.SetColLabelValue(4,"Next State")
-
-        
         
         self.Centre()
 
+        self.added_tapes = []
         
-   
     def closewindow(self,event):
         self.Destroy()
+
+    def add_tape(self, event):
+        tape = wx.TextCtrl(self.pnl1)
+        self.added_tapes.append(tape)
+        self.vbox.Add(tape,0,wx.EXPAND | wx.ALL, 10)
+        self.vbox.Layout()
+
+    def remove_tape(self, event):
+        if len(self.added_tapes) > 0:
+            last_tape = self.added_tapes.pop()
+            self.vbox.Remove(last_tape)
+            last_tape.Destroy()
+            self.vbox.Layout()
 
 if __name__=='__main__':
     app=wx.PySimpleApp()
