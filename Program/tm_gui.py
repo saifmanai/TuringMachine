e4e37a137cb7a2d40d0e854b93b6168b64b6dd8b
AzuraMeta <azura.meta@gmail.com>
2014-05-13 17:29:09 +0300

    Combined the main and gui files

--- Program/tm_gui.py
+++ Program/tm_gui.py
@@ -1,8 +1,8 @@
 
-#gui.py: Defines the graphical interface
-
 import wx
 import random
+import tm
+import programs
 
 BORDER = 2
 CURRENT_POS_STYLE = wx.TextAttr(wx.Colour(0, 0, 0), colBack=wx.Colour(0, 200, 255))
@@ -15,27 +15,32 @@ def make_layout_panel(parent, direction=wx.HORIZONTAL, *args, **kwargs):
     return panel
 
 
-class MainWindow(wx.Frame):
+class Main(wx.Frame):
     def __init__(self, parent=None, id=wx.ID_ANY):
         wx.Frame.__init__(self, parent, id, 'Turing Machine', size=(660,440), style=wx.SYSTEM_MENU|wx.CAPTION|wx.CLOSE_BOX) #style used to disable window resizing
         self.Centre()
         self.SetSizer(wx.BoxSizer(wx.VERTICAL))
 
-        
+
         controls_panel = make_layout_panel(self, wx.HORIZONTAL)
         self.Sizer.Add(controls_panel, 0, wx.EXPAND)
+
 
         program_label = wx.StaticText(controls_panel, label="Program: ") 
         controls_panel.Sizer.Add(program_label, 0, wx.ALIGN_CENTER_VERTICAL|wx.ALL, BORDER)
         
         program_chooser = wx.Choice(controls_panel)
+        for program in programs.plist:
+            program_chooser.Append(program.name)
         controls_panel.Sizer.Add(program_chooser, 0, wx.EXPAND|wx.ALL, BORDER)
+
         
         speed_label = wx.StaticText(controls_panel, label="Simulation speed (OPs/sec): ") 
         controls_panel.Sizer.Add(speed_label, 0, wx.ALIGN_CENTER_VERTICAL|wx.ALL, BORDER)
         
         speed_input = wx.SpinCtrl(controls_panel, min=1, initial=8, value='8', size=(60,-1))
         controls_panel.Sizer.Add(speed_input, 0, wx.EXPAND|wx.ALL, BORDER)
+
 
         run_pause_button = wx.Button(controls_panel, label="Run")
         controls_panel.Sizer.Add(run_pause_button, 0, wx.EXPAND|wx.ALL, BORDER)
@@ -60,24 +65,25 @@ class MainWindow(wx.Frame):
         main_panel.Sizer.Add(program_table, 1, wx.EXPAND)
 
 
+        self.Bind(wx.EVT_CHOICE, self.change_program, program_chooser)
+        self.Bind(wx.EVT_BUTTON, self.run_pause, run_pause_button)
+        self.Bind(wx.EVT_BUTTON, self.reset, reset_button)
+        
         self.program_chooser = program_chooser
         self.speed_input = speed_input
         self.run_pause_button = run_pause_button
         self.reset_button = reset_button
         self.tapes_panel = tapes_panel
         self.program_table = program_table
-        self.tapes = list()
-
-
-    def set_alphabet(self, alphabet):
-        self.alphabet = alphabet
+        
+        self.tape_panels = list()
 
 
     def add_tape(self, value):
         tape_row = make_layout_panel(self.tapes_panel, wx.HORIZONTAL)
         self.tapes_panel.Sizer.Add(tape_row, 0, wx.EXPAND|wx.ALL, BORDER)
 
-        label = wx.StaticText(tape_row, label="Tape #%d: " % (len(self.tapes)+1)) 
+        label = wx.StaticText(tape_row, label="Tape #%d: " % (len(self.tape_panels)+1)) 
         tape_row.Sizer.Add(label, 1, wx.ALIGN_CENTER_VERTICAL)
 
         tape_entry = wx.TextCtrl(tape_row, value=value, style=wx.TE_RICH2, size=(100,-1)) 
@@ -90,29 +96,28 @@ class MainWindow(wx.Frame):
             random.seed()
             value = list()
             for i in range(random.randint(4, 10)):
-                value.append(random.choice(self.alphabet))
+                value.append(random.choice(self.program.alphabet))
             tape_entry.SetValue(''.join(value))
         tape_row.Bind(wx.EVT_BUTTON, value_generator, gen_button)
 
-        self.tapes.append(tape_entry)
+        self.tape_panels.append(tape_entry)
 
-    def clear_tapes(self):
+    def reload_tapes(self):
         self.tapes_panel.Sizer.Clear(True)
-        self.tapes = list()
-
-    def enable_tapes(self, bool_enable):
-        self.tapes_panel.Enable(bool_enable)
-
-    def get_tape_value(self, tape_nr):
-        return self.tapes[tape_nr].GetValue()
+        self.tape_panels = list()
+        
+        for tape in self.program.tapes:
+            self.add_tape(''.join(tape))
+        self.tapes_panel.Layout()
 
     def update_tape(self, tape_nr, value, current_pos):
-        tape_entry = self.tapes[tape_nr]
+        tape_entry = self.tape_panels[tape_nr]
         tape_entry.SetValue("")
         tape_entry.SetValue(value)
         
         if current_pos != -1:
             tape_entry.SetStyle(current_pos, current_pos+1, CURRENT_POS_STYLE)
+
 
     def add_action(self, action):
         row = self.program_table.InsertStringItem(action['id'], action['state'])
@@ -120,10 +125,12 @@ class MainWindow(wx.Frame):
         self.program_table.SetStringItem(row, 2, ','.join(action['write_values']))
         self.program_table.SetStringItem(row, 3, ','.join(action['directions']))
         self.program_table.SetStringItem(row, 4, action['next_state'])
-        
-    def clear_actions(self):
-        self.program_table.DeleteAllItems()
 
+    def reload_program(self):
+        self.program_table.DeleteAllItems()
+        for action in self.program.actions:
+            self.add_action(action)
+    
     def select_action(self, action_id):
         for id in range(0, self.program_table.GetItemCount()):
             self.program_table.Select(id, False)
@@ -133,12 +140,76 @@ class MainWindow(wx.Frame):
             self.program_table.EnsureVisible(action_id)
 
 
-#test code to view the interface
+    def change_program(self, event):
+        program_id = event.GetSelection()
+        self.program = programs.plist[program_id]
+        
+        self.reload_tapes()
+        self.reload_program()
+    
+    
+    def run_pause(self, event):
+        if hasattr(self, 'tm') and self.tm.running:
+            if self.tm.should_continue.is_set():
+                self.tm.should_continue.clear()
+                
+                self.speed_input.Enable(True)
+                self.run_pause_button.SetLabel("Resume")
+            else:
+                self.tm.speed = self.speed_input.GetValue()
+                self.tm.should_continue.set()
+                
+                self.speed_input.Enable(False)
+                self.run_pause_button.SetLabel("Pause")
+        elif self.program_chooser.GetSelection() != wx.NOT_FOUND:
+                self.program_chooser.Enable(False)
+                self.speed_input.Enable(False)
+                self.run_pause_button.SetLabel("Pause")
+                self.reset_button.Enable(True)
+                self.tapes_panel.Enable(False)
+
+                for tape_nr, tape in enumerate(self.program.tapes):
+                    self.program.tapes[tape_nr] = list(self.tape_panels[tape_nr].GetValue())
+
+                self.tm = tm.TuringMachine(self.program, self.speed_input.GetValue(), self.tm_listener)
+                self.tm.start()
+                    
+    def reset(self, event=None):
+        if hasattr(self, 'tm'):
+            self.tm.running = False
+
+        self.program_chooser.Enable(True)
+        self.speed_input.Enable(True)
+        self.run_pause_button.SetLabel("Run")
+        self.reset_button.Enable(False)
+        self.tapes_panel.Enable(True)
+        self.select_action(-1)
+        
+        for tape_nr, tape in enumerate(self.tm.program.tapes):
+            self.update_tape(tape_nr, ''.join(tape), -1)
+
+
+    def tm_listener(self, tm, state):
+        wx.CallAfter(self.window_updater, tm, state)
+    
+    def window_updater(self, tm, state):
+        if hasattr(tm, 'error'):
+            wx.MessageBox(str(tm.error), 'Error', wx.OK|wx.ICON_ERROR)
+            self.reset()
+        else:
+            if tm.running:
+                for tape_nr, tape in enumerate(tm.program.tapes):
+                    self.update_tape(tape_nr, ''.join(tape), tm.tapes_pos[tape_nr])
+                
+                if hasattr(tm, 'action') and tm.action != None:
+                    self.select_action(tm.action['id'])
+            else:
+                self.reset()
+
+
+
 if __name__ == '__main__':
     app = wx.App()
-    window = MainWindow()
-    window.set_alphabet("01")
-    window.add_tape("1011")
-    window.add_tape("1001011")
+    window = Main()
     window.Show()
     app.MainLoop()
